3ae57eb4e8e72cf0d54e8b043427690325c9a5cf
Artur Klimov <evklimow@yandex.ru>
2017-12-07 20:57:39 +0300

    Initial

---
+++ SuperPuperProject/SuperPuperProject/SuperPuperProject.cpp
@@ -0,0 +1,49 @@
+// SuperPuperProject.cpp: определ€ет точку входа дл€ консольного приложени€.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	int count; // переменна€ дл€ выбора в switch
+	double a, b; // переменные дл€ хранени€ операндов
+	cout << "Vvedite pervoe chislo: ";
+	cin >> a;
+	cout << "Vvedite vtoroe chislo: ";
+	cin >> b;	
+	cout << "Vibirite deistvie: 1-clojenie; 2-vichitanie; 3-ymnojenie; 4-delenie: ";
+	cin >> count;
+	switch (count) // начало оператора switch
+	{
+		case 1: 
+		{
+			//–азрабатываетс€
+			break;
+		}
+		case 2: 
+		{
+			//–азрабатываетс€
+			break;
+		}
+		case 3: 
+		{
+			//–азрабатываетс€
+			break;
+		}
+		case 4: 
+		{
+			//–азрабатываетс€
+			break;
+		}
+		default: // если count равно любому другому значению
+		cout << "Nepravilni vvod" << endl;
+	}
+	system("pause");
+	return 0;
+}
+
